a48b4a55b89409d490ec2e93ee3c7ed7614472da
Sharif Hasan <me.sharif.hasan@gmail.com>
2023-12-09 02:25:09 +0600

    Added build

--- public/build/assets/vue-16c48d32.js
+++
@@ -1,1 +0,0 @@
-import{bD as o,bE as a,bF as s,o as n,bG as e,bH as c,bI as p}from"./router-8c4102b6.js";o.add(a,s);const t=n(p);t.component("font-awesome-icon",e).use(c).mount("#app");
---
+++ public/build/assets/vue-6c8f5d11.js
@@ -0,0 +1,1 @@
+import{bD as o,bE as a,bF as s,o as n,bG as e,bH as c,bI as p}from"./router-0c133be8.js";o.add(a,s);const t=n(p);t.component("font-awesome-icon",e).use(c).mount("#app");
